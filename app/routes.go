81d1a24908731553b9bccc1eb3ce79d5f78b3cad
teneieiza <tenmonsterten@gmail.com>
2025-09-25 13:31:42 +0700

    Add route.go for api endpoint

---
+++ app/routes.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"time"
+
+	"github.com/Teneieiza/go-spinsolf-test/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterRoutes(app *fiber.App) {
+	api := app.Group("/api")
+
+	// Stations
+	api.Get("/stations/nearby", controllers.GetNearbyStations)
+	api.Get("/stations/nearby/paginated", controllers.GetNearbyStationsPaginated)
+	api.Post("/stations/import/url", controllers.ImportUrlStations)
+	api.Post("/stations/import/file", controllers.ImportFileStations)
+
+	// health check
+	api.Get("/health", func(c *fiber.Ctx) error {
+			return c.JSON(fiber.Map{
+					"status": "ok",
+					"timestamp": time.Now().Format(time.RFC3339),
+			})
+	})
+}
